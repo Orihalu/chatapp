69318dcd5171e0a2a2ff0d673686314ac2cdeb5a
vagrant <vagrant@localhost.localdomain>
2018-07-20 03:10:27 +0100

    userprpfpage

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,13 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
+<a href="{{ url('/home')}}" class="row justify-content-center">TOPPAGE</a>
 <div class="container">
-  <div class="card">
-    {{--{{dd($user->name)}}--}}
-    <h1>{{ $user->name }}</h1>
-  </div>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @elseif (session('danger'))
+        <div class="alert alert-danger" role="alert">
+            {{ session('danger') }}
+        </div>
+    @endif
+    <div class="card">
+      <div class="card-body">
+        {{$user->following}}
+        <h1>{{ $user->name }}</h1>
+        @if(Auth::user()->following->contains($user->id))
+        <form method="post" action="{{ action('UserController@unfollow',$user) }}">
+          {{ csrf_field() }}
+          <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+          <div class="form-group">
+            <button type="submit" style="float:right;" class="btn btn-danger">unfollow</button>
+          </div>
+        </form>
+        @else(Auth::user()->following->contains($user->id))
+        <form method="post" action="{{ action('UserController@follow',$user) }}">
+          {{ csrf_field() }}
+          <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+          <div class="form-group">
+            <button type="submit" style="float:right;" class="btn btn-success">follow</button>
+          </div>
+        </form>
+        @endif
+      </div>
+    </div>
+    @forelse($user->following as $following)
+    @if($following->id == Auth::user()->id)
+    @continue
+    @elseif(Auth::user()->following->contains($following->id))
+        <div class="card">
+          <div class="card-body">
+        <a href="{{ action('UserController@show', $following) }}">{{$following->name}}</a>
+        <form method="post" action="{{ action('UserController@unfollow',$following) }}">
+          {{ csrf_field() }}
+          <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+          <div class="form-group">
+            <button type="submit" style="float:right;" class="btn btn-danger">unfollow</button>
+          </div>
+        </form>
+          </div>
+        </div>
+    @else
+        <div class="card">
+          <div class="card-body">
+            <a href="{{ action('UserController@show', $following) }}">{{$following->name}}</a>
+            <form method="post" action="{{ action('UserController@follow',$following) }}">
+              {{ csrf_field() }}
+              <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            <div class="form-group">
+              <button type="submit" style="float:right;" class="btn btn-success">follow</button>
+            </div>
+            </form>
+          </div>
+        </div>
+    @endif
+    @empty
+      <div class="card">
+        <div class="card-body">
+      <li>daremoinai</li>
+        </div>
+      </div>
+    @endforelse
 </div>
-
-
 
 @endsection
